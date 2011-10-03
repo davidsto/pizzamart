e874c53595c155684a0501b944a29f372e1d3c36
sigurlu <sigurd@sigurd-laptop.(none)>
2011-10-03 11:43:56 +0200

    Script for inserting dummy datainto db

---
+++ dummydata.sql
@@ -0,0 +1,22 @@
+INSERT INTO kunde (fornavn, etternavn, tlf, adresse, postnr)
+	values ('Ola', 'Nordmann', '12345678', 'Munkegata 1', '7050');
+INSERT INTO kunde (fornavn, etternavn, tlf, adresse, postnr)
+	values ('Sigurd', 'Lund', '11223344', 'Odd Brochmannsvei 49', '7051');
+INSERT INTO kunde (fornavn, etternavn, tlf, adresse, postnr)
+	values ('Even', 'Lislebø', '12345688', 'Munkegata 5', '7050');
+INSERT INTO kunde (fornavn, etternavn, tlf, adresse, postnr)
+	values ('Øivind', 'Binde', '11345678', 'Munkegata 9', '7050');
+
+
+INSERT INTO ansatt (fornavn, etternavn, tlf, adresse, postnr)
+	values ('David', 'Storjord', '33345678', 'Munkegata 19', '7050');
+
+
+INSERT INTO ordre (status)
+	values ('Levert');
+
+
+INSERT INTO vare (navn, pris)
+	values ('Pepperoni Stor', '220');
+INSERT INTO vare (navn, pris)
+	values ('Pepperoni Liten', '150');
