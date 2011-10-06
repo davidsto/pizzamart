227f2a68224a4f958cb54590d3109875690ed789
sigurlu <sigurd@sigurd-laptop.(none)>
2011-10-06 16:21:40 +0200

    jobbet litt videre

--- Prosjekt/src/Database/Test.java
+++ Prosjekt/src/Database/Test.java
@@ -1,26 +1,46 @@
 package Database;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import Class.Employee;
+
 public class Test {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, IOException {
 		
 		
 		Database db = Database.getDatabase();
 		System.out.println("Database kobling er opprettet");
 		Connection con = db.getConnection();
-
 		
-		System.out.println(db.getConnection());
-		System.out.println(db.select("SELECT etternavn FROM ansatt"));
-		db.insert("INSERT into ansatt (fornavn, etternavn, adresse, postnr, tlf) values('Per', 'Ludviksen', 'Høgskoloeringen 3', '7050','87654321') ");
-		System.out.println(db.select("SELECT etternavn FROM ansatt"));
+		
+		BufferedReader br = new BufferedReader(new FileReader("/home/sigurd/dummydata.sql"));
+		
+		ScriptRunner sr = new ScriptRunner(con, false, true);
+		sr.runScript(br);
+//
+//		
+//		System.out.println(db.getConnection());
+//		System.out.println(db.select("SELECT etternavn FROM ansatt"));
+//		db.insert("INSERT into ansatt (fornavn, etternavn, adresse, postnr, tlf) values('Per', 'Ludviksen', 'Høgskoloeringen 3', '7050','87654321') ");
+//		System.out.println(db.select("SELECT etternavn FROM ansatt"));
 
-		con.close();
+//		con.close();
+		
+		Employee emp = new Employee("heiii", "Ola", "Nordmann");
+		ArrayList<String> list = new ArrayList<String>();
+		ResultSet rs = db.select("SELECT * FROM employee");
+		
+		while (rs.next())
+			System.out.println(rs.getString(2));
 
 	}
 	
