a8ea6049b2d434952b5f4a5553fb73fee86e4cff
sigurlu <sigurd@sigurd-laptop.(none)>
2011-10-18 14:07:19 +0200

    rettet opp feil i customer query

--- Prosjekt/src/Class/Order.java
+++ Prosjekt/src/Class/Order.java
@@ -2,7 +2,6 @@ package Class;
 
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import Database.Database;
@@ -10,38 +9,18 @@ import Database.Database;
 public class Order {
 	
 	private Customer customer;
-	private HashMap<Product, Integer> productsInOrder;
+	private HashMap<Product, Integer> products;
 	private String status;
 	private String idorder;
 	
 	public Order(Customer customer, HashMap<Product, Integer> products, String status){
 		this.customer = customer;
-		this.productsInOrder = products;
+		this.products = products;
 		this.status = status;
 	}
 	
 	public Order(Customer customer){
 		this.customer = customer;
-	}
-	public HashMap<Product, Integer> getProductsFromOrdre(){
-		return this.productsInOrder;
-	}
-	
-	public double getOrderTotalPrice(Order order){
-		double totalprice = 0;
-	    Iterator it = productsInOrder.entrySet().iterator();
-	    while(it.hasNext()){
-	    	Product p = (Product) it.next();
-	    	int quantity = productsInOrder.get(p);	
-	        totalprice += p.getPrice()*quantity;
-	    }
-		return totalprice;
-	}
-	
-	public int getProductQuanta(Product product){
-	    if(productsInOrder.containsKey(product))
-	    	return productsInOrder.get(product);
-	    return 0;
 	}
 	
 	public void addOrderToDatabase(){
@@ -56,7 +35,7 @@ public class Order {
 	}
 	
 	public void addProductToOrder(Product product, int quantity){
-		productsInOrder.put(product, quantity);
+		products.put(product, quantity);
 	}
 	
 	public void setStatus(String status){
@@ -71,6 +50,7 @@ public class Order {
 		}
 		
 	}
+	
 	
 	
 
